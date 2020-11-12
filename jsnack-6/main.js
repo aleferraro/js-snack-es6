0f2e340e36dc6e5e0827f47587ce5a053d21d9ac
Alessandro Ferraro <ale_ferraro@libero.it>
2020-11-12 18:18:33 +0100

    JSnack 6

---
+++ jsnack-6/main.js
@@ -0,0 +1,45 @@
+/*
+jsnack 6
+dato questo array di obj iniziale:
+var arrayObj = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
+Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array)
+una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale
+
+*/
+
+const arrayObj = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+//creo un nuovo array aggiungendo ad ogni elemento la proprietà position che contiene una lettera casuale presa dalla variabile alphabet
+const newArray = arrayObj.map((element) => {
+  element.position = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+  return element
+});
+
+console.log(newArray);
+
+const resultEl = document.getElementById('result');
+
+//stampo il nuovo array nell'HTML
+arrayObj.forEach((element, index) => {
+  resultEl.innerHTML += `
+  <h2>Object ${index}</h2>
+  <p>
+    Nome: ${element.name}<br>
+    Tipo: ${element.type}<br>
+    Colore: ${element.color}<br>
+    Posizione: ${element.position}<br>
+  </p>
+  `
+});
